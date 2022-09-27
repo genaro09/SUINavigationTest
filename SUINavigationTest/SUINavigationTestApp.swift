fb0aef7b3fe77973bc8bfb328fa38624ba82be08
Genaro Alvarenga <gena_albert@hotmail.com>
2022-09-27 11:09:00 -0600

    feat: Create xcode project

---
+++ SUINavigationTest/SUINavigationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUINavigationTestApp.swift
+//  SUINavigationTest
+//
+//  Created by Genaro Alvarenga on 9/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SUINavigationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
